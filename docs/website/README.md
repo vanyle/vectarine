aabc3891a93109846ac6ea69b549ad153c7b5eab
Antoine Delègue <delegue.antoine@gmail.com>
2025-12-27 10:42:16 +0100

    :tada: docs website

---
+++ docs/website/README.md
@@ -0,0 +1,15 @@
+# Vectarine Documentation Website
+
+[![Built with Starlight](https://astro.badg.es/v2/built-with-starlight/tiny.svg)](https://starlight.astro.build)
+
+## Commands
+
+| Command                   | Action                                           |
+| :------------------------ | :----------------------------------------------- |
+| `pnpm install`             | Installs dependencies                            |
+| `pnpm dev`             | Starts local dev server at `localhost:4321`      |
+| `pnpm build`           | Build your production site to `./dist/`          |
+| `pnpm preview`         | Preview your build locally, before deploying     |
+| `pnpm astro ...`       | Run CLI commands like `astro add`, `astro check` |
+| `pnpm astro -- --help` | Get help using the Astro CLI                     |
+
---
+++ docs/website/astro.config.mjs
@@ -0,0 +1,49 @@
+// @ts-check
+import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [
+        starlight({
+            title: "Vectarine",
+            favicon: "./src/assets/vectarine.png",
+            customCss: ["./src/styles/custom.css"],
+            components: {
+                SocialIcons: "./src/components/SocialIcons.astro",
+            },
+            social: [
+                {
+                    icon: "github",
+                    label: "GitHub",
+                    href: "https://github.com/vanyle/vectarine",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Welcome",
+                    link: "/",
+                },
+                {
+                    label: "Guides",
+                    items: [
+                        {
+                            label: "Create your first game",
+                            link: "/guides/getting-started/",
+                        },
+                        {
+                            label: "Optimizing your game",
+                            link: "/guides/use-fastlists/",
+                        },
+                        {
+                            label: "Create a native plugin",
+                            link: "/guides/create-a-plugin/",
+                        },
+                    ],
+                },
+            ],
+        }),
+        react(),
+    ],
+});
