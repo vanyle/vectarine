6301afa3853edad906224ae743cc146ef206413a
Antoine Delègue <delegue.antoine@gmail.com>
2026-07-31 01:07:03 +0200

    :tada: auto-generate vscode settings when creating a new project

--- vectarine-cli/src/project/createproject.rs
+++ vectarine-cli/src/project/createproject.rs
@@ -36,6 +36,16 @@ static DEFAULT_LUAURC: &str = r#"{
 	}
 }"#;
 
+static DEFAULT_VSCODE_SETTINGS: &str = r#"{
+	"luau-lsp.platform.type": "standard",
+	"luau-lsp.sourcemap.enabled": false,
+	"luau-lsp.sourcemap.autogenerate": false,
+    "[luau]": {
+		"editor.defaultFormatter": "JohnnyMorganz.stylua",
+		"editor.formatOnSave": true,
+	},
+}"#;
+
 fn copy_default_luau_api(project_folder: &Path) -> Result<(), std::io::Error> {
     let luau_api_path = project_folder.join("luau-api");
     let reference_luau_api_path = get_luau_api_path();
@@ -45,6 +55,7 @@ fn copy_default_luau_api(project_folder: &Path) -> Result<(), std::io::Error> {
 pub fn create_game_and_get_path(game_name: &str, game_path: &Path) -> anyhow::Result<PathBuf> {
     let project_folder = game_path.join(game_name);
     let project_file_path = project_folder.join("game.vecta");
+    let vscode_settings_path = project_folder.join(".vscode/settings.json");
     let script_folder = project_folder.join("scripts");
     let project_info = ProjectInfo {
         title: game_name.to_string(),
@@ -68,6 +79,13 @@ pub fn create_game_and_get_path(game_name: &str, game_path: &Path) -> anyhow::Re
     setup_failed = setup_failed.or(fs::write(&main_script_path, DEFAULT_CODE).err());
     setup_failed = setup_failed.or(copy_default_luau_api(&project_folder).err());
     setup_failed = setup_failed.or(fs::write(project_folder.join(".luaurc"), DEFAULT_LUAURC).err());
+    setup_failed = setup_failed.or(fs::create_dir_all(
+        vscode_settings_path
+            .parent()
+            .expect("The parent of .vscode/settings.json should exist"),
+    )
+    .err());
+    setup_failed = setup_failed.or(fs::write(&vscode_settings_path, DEFAULT_VSCODE_SETTINGS).err());
 
     if let Some(setup_failed) = setup_failed {
         return Err(anyhow::anyhow!(
