44a1a270cd899c738b7044facf57cb3f0691fac8
Antoine Delègue <delegue.antoine@gmail.com>
2026-08-18 00:53:01 +0200

    :construction_worker: improve the design of the new project screen

--- vectarine-cli/src/features/createproject.rs
+++ vectarine-cli/src/features/createproject.rs
@@ -3,8 +3,17 @@ use std::path::Path;
 use runtime::anyhow;
 use std::path::PathBuf;
 
-use crate::project::createproject::create_game_and_get_path;
+use crate::project::createproject::{
+    ProjectCreationOptions, StartingProjectTemplate, create_game_and_get_path,
+};
 
 pub fn create_project(project_path: &Path, game_name: &str) -> anyhow::Result<PathBuf> {
-    create_game_and_get_path(game_name, project_path)
+    let options = ProjectCreationOptions {
+        name: game_name.to_string(),
+        project_location: project_path.to_path_buf(),
+        init_git_repo: true,
+        init_vs_settings: true,
+        template: StartingProjectTemplate::FromScratch,
+    };
+    create_game_and_get_path(&options)
 }
